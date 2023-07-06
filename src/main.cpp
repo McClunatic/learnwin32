96962d1fde2fa437e5b434039e38be85bb3252a9
Brian McClune <brian.mcclune@unnpp.gov>
2023-07-06 07:56:39 -0400

    Reproduce pybind11 _COM_SMARTPTR_TYPEDEF error

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <pybind11/pybind11.h>
+#include <shobjidl.h>
+#include <comdef.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 int add(int i, int j) {
     return i + j;
+}
+
+_COM_SMARTPTR_TYPEDEF(IFileOpenDialog, __uuidof(IFileOpenDialog));
+
+IFileOpenDialogPtr getPointer() {
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+        COINIT_DISABLE_OLE1DDE);
+    if (SUCCEEDED(hr))
+    {
+        IFileOpenDialog *pFileOpen;
+
+        // Create the FileOpenDialog object.
+        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+                IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+        IFileOpenDialogPtr smartFileOpen(pFileOpen);
+        return smartFileOpen;
+    }
 }
 
 namespace py = pybind11;
@@ -36,4 +56,10 @@ PYBIND11_MODULE(learnwin32, m) {
     )pbdoc");
 
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+
+    m.def("getPointer", &getPointer, R"pbdoc(
+        Get a pointer.
+
+        Some other explanation about the getPointer function.
+    )pbdoc");
 }
