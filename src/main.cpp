2cd37a0214f965ff547c7072a7ba9c1c7c8157b2
Brian McClune <brian.mcclune@unnpp.gov>
2023-08-08 19:53:32 -0400

    Demonstrate working wrapper class for pybind11

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,51 @@
 #include <pybind11/pybind11.h>
-#include <shobjidl.h>
 #include <comdef.h>
+#include <shobjidl.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 _COM_SMARTPTR_TYPEDEF(IFileOpenDialog, __uuidof(IFileOpenDialog));
 
-IFileOpenDialogPtr getPointer() {
-    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
-        COINIT_DISABLE_OLE1DDE);
-    if (SUCCEEDED(hr))
-    {
-        IFileOpenDialog *pFileOpen;
+// https://learn.microsoft.com/en-us/windows/win32/cossdk/interpreting-error-codes
+void ErrorDescription(HRESULT hr)
+{
+     if(FACILITY_WINDOWS == HRESULT_FACILITY(hr))
+         hr = HRESULT_CODE(hr);
+     TCHAR* szErrMsg;
 
-        // Create the FileOpenDialog object.
-        hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
-                IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
-
-        IFileOpenDialogPtr smartFileOpen(pFileOpen);
-        return smartFileOpen;
-    }
+     if(FormatMessage(
+       FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+       NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+       (LPTSTR)&szErrMsg, 0, NULL) != 0)
+     {
+         _tprintf(TEXT("%s"), szErrMsg);
+         LocalFree(szErrMsg);
+     } else
+         _tprintf( TEXT("[Could not find a description for error # %#x.]\n"), hr);
 }
+
+class FileOpenDialog {
+private:
+    IFileOpenDialogPtr com;
+public:
+    static HRESULT CoInitializeEx() {
+        return ::CoInitializeEx(
+            NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+    }
+
+    HRESULT CoCreateInstance() {
+        HRESULT res;
+        res = ::CoCreateInstance(
+            CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_PPV_ARGS(&com));
+        if (FAILED(res))
+            ErrorDescription(res);
+        return res;
+    }
+};
 
 namespace py = pybind11;
 
@@ -39,27 +59,17 @@ PYBIND11_MODULE(learnwin32, m) {
         .. autosummary::
            :toctree: _generate
 
-           add
-           subtract
+           FileOpenDialog
+           FileOpenDialog.CoInitializeEx
+           FileOpenDialog.CoCreateInstance
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
+    py::class_<FileOpenDialog>(m, "FileOpenDialog", "FileOpenDialog class.")
+        .def(py::init<>())
+        .def_static("CoInitializeEx", &FileOpenDialog::CoInitializeEx,
+                    "The CoInitializeEx static method.")
+        .def("CoCreateInstance", &FileOpenDialog::CoCreateInstance,
+             "The CoCreateInstance method.");
 
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-
-    m.def("getPointer", &getPointer, R"pbdoc(
-        Get a pointer.
-
-        Some other explanation about the getPointer function.
-    )pbdoc");
 }
